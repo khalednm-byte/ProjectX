06440343fa03e590a50081fb7150507dede55353
Mercury <khalednoilati@gmail.com>
2023-07-31 19:00:08 +0300

    Updated version of main

--- projectX.c
+++ projectX.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-//char names();
-//int ages();
-int IDnumber();
-//void Files();
+
+int IDnumber(FILE *fptr);
+void dataBase(int age, char names[100], FILE *fptr);
 //FILE *fptr;
 
 int main(){
@@ -14,6 +13,8 @@ int main(){
     //int rand = 10000;
     char names[100];
     int ages;
+    FILE *fptr; //file to hold all system information
+
 
 //Name entry
 printf("Enter Name : ");
@@ -25,9 +26,11 @@ scanf("%d", &ages);
 
 printf("Welcome %s\nYou're %d years old\n", names, ages);
 
-//Generate an ID number for user that starts with the last two digits of the year registered 
-IDnumber();
+//Appends all info to system DataBase
+dataBase(ages, names, fptr) ;
 
+//Generate an ID number for user that starts with the last two digits of the year registered 
+IDnumber(fptr);
 
 return 0;
 
@@ -35,11 +38,26 @@ return 0;
 
 }
 
-int IDnumber(){
+int IDnumber(FILE *fptr){
     srand(time(NULL)); // Initialize the random number generator with the current time
 
     int number = 23000000 + rand() % 1000000; // Generate a random number between 23000000 and 23999999
 
     printf("Your generated number is %08d\n", number); // Print the number with leading zeros to make it 8 digits
 
+    fptr = fopen("dataB.txt", "a");
+    fprintf(fptr,"ID = %08d\n==============================\n", number);
+    fclose(fptr) ;
+
+    
+
+}
+
+void dataBase(int age, char names[100], FILE *fptr){
+    
+    fptr = fopen("dataB.txt", "a");
+
+    fprintf(fptr, "\n==============================\nName : %s\nAge : %d\n", names, age);
+
+    fclose(fptr) ;
 }
